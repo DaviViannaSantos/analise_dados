686a71f739f31061cec642f4d9ab2327d8ef6bc0
DaviViannaSantos <22402098@aluno.cotemig.com.br>
2026-09-03 08:58:39 -0300

    exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
 
-# 1. Vetor NumPy com as idades
 idades = np.array([18, 21, 19, 22, 20])
 print("Vetor de idades:", idades)
 
-# 2. Matriz 2x3 qualquer
 matriz = np.array([
     [1, 2, 3],
     [4, 5, 6]
@@ -13,7 +11,6 @@ matriz = np.array([
 print("Shape da matriz:", matriz.shape)
 print("Elemento [1, 2]:", matriz[1, 2])
 
-# 3. Series com índice nomeado
 series_idades = pd.Series(
     idades, 
     index=["Ana", "Bruno", "Carla", "Diego", "Eva"], 
@@ -21,8 +18,6 @@ series_idades = pd.Series(
 )
 print("\nSeries de idades:\n", series_idades)
 
-# 4. Usando loc para mostrar a idade da Carla
 print("\nIdade da Carla (loc):", series_idades.loc["Carla"])
 
-# 5. Filtrando quem tem 21 anos ou mais
 print("\nPessoas com 21 anos ou mais:\n", series_idades[series_idades >= 21])
